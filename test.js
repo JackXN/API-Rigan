61fe458c9022a5ca0ab105dc4c136bdf468ca394
Jack Rigan <73765650+JackXN@users.noreply.github.com>
2021-12-06 06:22:20 -0700

    Icon testing

---
+++ test.js
@@ -0,0 +1,23 @@
+{"campaigns":[
+
+    {"id":1,"campaign_name":"Tiktok","campaign_icon_url":"https://is1-ssl.mzstatic.com/image/thumb/Purple118/v4/11/d7/70/11d770fa-14d6-e6ee-b118-c273bcf91906/AppIcon_TikTok-1x_U007emarketing-85-220-0-5.png/230x0w.jpg","pay_per_install":"19.8","medias":[{"cover_photo_url":"https://firebasestorage.googleapis.com/v0/b/plugcoinprod.appspot.com/o/tiktok%2F44f871ab-a7de-4bcc-9ecf-4ecadbc5bc83.png?alt=media&token=ea854bfa-5fb7-4b23-b85c-312ae9860338","download_url":"https://firebasestorage.googleapis.com/v0/b/plugcoinprod.appspot.com/o/tiktok%2F44f871ab-a7de-4bcc-9ecf-4ecadbc5bc83.mp4?alt=media&token=9580e0d2-f07e-4c78-bcab-d1b26d2c525a","tracking_link":"www.plugco.in/tiktok/2","media_type":"video"},
+
+
+
+
+    
+    
+    
+    {"cover_photo_url":"https://firebasestorage.googleapis.com/v0/b/plugcoinprod.appspot.com/o/tiktok%2F1403e07a-24f0-4fef-9022-ffc433b0e767.png?alt=media&token=cf325104-5736-41e9-b79d-08644f79da8b","download_url":"https://firebasestorage.googleapis.com/v0/b/plugcoinprod.appspot.com/o/tiktok%2F1403e07a-24f0-4fef-9022-ffc433b0e767.mp4?alt=media&token=e11aa682-482e-45f5-875e-06c60da8f04b","tracking_link":"www.plugco.in/tiktok/2","media_type":"video"},{"cover_photo_url":"https://firebasestorage.googleapis.com/v0/b/plugcoinprod.appspot.com/o/tiktok%2F47766c1f-e280-428f-a22d-e9cf8765e259.png?alt=media&token=ae393f4e-ca3f-42d7-aa55-25f3285d73c5","download_url":"https://firebasestorage.googleapis.com/v0/b/plugcoinprod.appspot.com/o/tiktok%2F47766c1f-e280-428f-a22d-e9cf8765e259.mp4?alt=media&token=3adb2011-a671-4ff0-9230-3d6c3b16ad97","tracking_link":"www.plugco.in/tiktok/2","media_type":"video"},{"cover_photo_url":"https://firebasestorage.googleapis.com/v0/b/plugcoinprod.appspot.com/o/tiktok%2F5868ea0f-abd2-495b-a8bc-cf169d547d13.png?alt=media&token=46347dca-1a68-41dc-8d0c-2fcffd8bb9ed","download_url":"https://firebasestorage.googleapis.com/v0/b/plugcoinprod.appspot.com/o/tiktok%2F5868ea0f-abd2-495b-a8bc-cf169d547d13.mp4?alt=media&token=ea11ceef-371d-4e30-b7df-7694e63bd69f","tracking_link":"www.plugco.in/tiktok/2","media_type":"video"}]}
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
