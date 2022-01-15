29ff3c975c790c7be2f8dbeb54b44124197de24a
Jack Rigan <73765650+JackXN@users.noreply.github.com>
2022-01-15 15:24:59 -0700

    finished controllers for projects

--- models/Projects.js
+++ models/Projects.js
@@ -2,16 +2,17 @@ const mongoose = require('mongoose');
 
 const ProjectSchema = new mongoose.Schema(
     {
+        id: {type: Number, required: true, unique: true},
         title: {type: String, required: true, unique: true},
         backgroundColor: {type: String, required: true},
         descriptionTitleOne: {type: String, required: true},
         descriptionOne: {type: String, required: true},
-        descriptionTitleTwo: {type: String, required: true},
-        descriptionTwo: {type: String, required: true},
+        descriptionTitleTwo: {type: String, required: false},
+        descriptionTwo: {type: String, required: false},
         descriptionImgOne: {type: String, required: true},
-        descriptionImgTwo: {type: String, required: true},
-        iconOne: {type: String, required: true},
-        iconTwo: {type: String, required: true},
+        descriptionImgTwo: {type: String, required: false},
+        iconOne: {type: String, required: false},
+        iconTwo: {type: String, required: false},
         iconThree: {type: String, required: false},
         iconFour: {type: String, required: false}
     },
