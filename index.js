e66548f7ba63f7a537ff553387132f49cdc8fc4a
Jack Rigan <73765650+JackXN@users.noreply.github.com>
2022-01-15 16:23:09 -0700

    run

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
-const cors = require('cors')
+const cors = require('cors');
 
 // Routes 
 const projectRoute = require('./routes/projects');
